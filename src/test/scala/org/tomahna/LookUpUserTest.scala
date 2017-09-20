e6bfaf1f6230fc2a363db30bf34a40843461c41c
Kevin Rauscher <magnus@tomahna.fr>
2017-09-20 19:57:22 +0200

    test if keys are consecutives

---
+++ src/test/scala/org/tomahna/LookUpUserTest.scala
@@ -0,0 +1,16 @@
+package org.tomahna
+
+import org.apache.spark.rdd.RDD
+import org.scalatest.{FlatSpec, Matchers}
+import org.tomahna.SparkApp._
+
+class LookUpUserTest extends FlatSpec with Matchers {
+  "Aggregate" should "be unique by (user/product)" in {
+    val ratings = sc.textFile("xag.csv").flatMap(Rating.fromCSV).cache()
+    implicit val lookUpUser: RDD[LookUpUser] =
+      LookUpUser.userLookUpRDD(ratings)
+
+    val ids = lookUpUser.map(u => u.userIdAsLong).collect().sorted
+    ids should be(0l to ids.last)
+  }
+}
